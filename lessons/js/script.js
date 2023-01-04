5b35c912bef7030c9a3ed6257b81a11f307d8355
LoveKach <cryptonitik@gmail.com>
2023-01-04 09:03:55 +0300

    transform

---
+++ lessons/js/script.js
@@ -0,0 +1,10 @@
+console.log('Hello world');
+console.log("Hello");
+let myName = 'Valery';
+let myAge = '30';
+console.log('Мое имя: ' + myName);
+console.log('Мой возраст: ' + myAge +' лет');
+console.log(myAge + myName);
+console.log(`${myAge}  ${myName}`);
+console.log(`Мое имя:  ${myName}`);
+// alert('Hello');
