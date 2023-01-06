f816cc8fce8d1307107a4d7afc0d02823a27271e
LoveKach <cryptonitik@gmail.com>
2023-01-06 15:41:58 +0300

    interpolation

--- lessons/js/interpolation.js
+++ lessons/js/interpolation.js
@@ -2,3 +2,8 @@ const stark = 'Arya';
 const doYou = 'Do you want to eat';
 
 console.log(`${doYou}, ${stark}?`);
+
+
+// Извлечения символа из строки.
+const name = 'Na\nharis';
+console.log(name[7]);
