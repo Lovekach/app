71843701e01bd4d8742845e09c3cf75c5a245786
LoveKach <cryptonitik@gmail.com>
2023-01-06 14:37:56 +0300

    переменные и конкатенация

--- lessons/js/script.js
+++ lessons/js/script.js
@@ -1,10 +1,8 @@
-console.log('Hello world');
-console.log("Hello");
-let myName = 'Valery';
-let myAge = '30';
-console.log('Мое имя: ' + myName);
-console.log('Мой возраст: ' + myAge +' лет');
-console.log(myAge + myName);
-console.log(`${myAge}  ${myName}`);
-console.log(`Мое имя:  ${myName}`);
-// alert('Hello');
+let info = "We couldn't verify your mother's maiden name.";
+let intro = 'Here is important information about your account security.';
+
+let firstName = 'Joffrey';
+let greeting = 'Hello';
+
+console.log(greeting + ', ' + firstName + '!');
+console.log(intro + '\n' + info);
