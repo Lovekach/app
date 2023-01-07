515afc1f130aa40d8ef622562ebc4a40f93c6ccf
LoveKach <cryptonitik@gmail.com>
2023-01-07 09:46:37 +0300

    minin

---
+++ lessons/js/bond_1.js
@@ -0,0 +1,49 @@
+// 1 Переменные
+// variable
+var name = 'Valery';// string
+// const lastName = 'Zubov';//string
+let age =  30; //number
+const isProgrammer = true;//boolean
+
+name = 'Lera';
+console.log(name);
+
+age = 30;
+console.log(age);
+
+age = false;
+console.log(age); 
+
+age = 25;
+
+const fistName = 'Valery';
+const lastName1 = 'Zubov';
+
+const $ = 'some value';
+//const if = 'mket' // err
+
+const withNum5 = 5;
+// const 5withNum = 5 // err
+
+//2 Мутирование 
+// Конкатенация (соединение строк)
+
+// console.log('Имя человека: ' + name + ', а возраст:' + age );
+// alert('Имя человека: ' + firstName + 'а возраст человека: ' + age);
+
+
+// const lastName = prompt('Введите фамилию');
+// alert(fistName + ' ' + lastName);
+
+// 3 Операторы
+
+
+
+
+//camelCase
+// (firstName
+// lastName
+// oneGarden
+// twoGarden
+// oneGreenTree
+// twoGreenTree)
