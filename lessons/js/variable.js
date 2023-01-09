95848b3acf0d4049d366878d296c48c925c3f00d
LoveKach <cryptonitik@gmail.com>
2023-01-09 10:21:00 +0300

    boost

--- lessons/js/variable.js
+++ lessons/js/variable.js
@@ -1,8 +1,14 @@
 const carCoast = {
     carCoast: "Opel"
 }
-carCoast.new = "Mazda"
-console.log(carCoast)
+carCoast.new = "Mazda";
+console.log(carCoast);
 
-carCoast.popular = "Audi"
-console.log(carCoast)
+carCoast.popular = "Audi";
+console.log(carCoast);
+
+carCoast.year = 10;
+console.log(carCoast);
+
+carCoast.colorWhite = true;
+console.log(carCoast,carCoast.popular)
