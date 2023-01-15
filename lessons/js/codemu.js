1615748709ce3a5fd7cc5e7596bfaa953cb4199a
LoveKach <cryptonitik@gmail.com>
2023-01-15 20:42:37 +0300

    codemu

--- lessons/js/codemu.js
+++ lessons/js/codemu.js
@@ -1,25 +1,5 @@
 "use strict";
-alert('Привет,мир!');
-
-let a = 3;
+let a = 10,
 alert(a);
-
-let b;
-b = 3;
-alert(a);
-
-
-let num = 123;
-alert(num);
-
-
-let a = 1;
-let b = 2;
-let c = 3;
-
-let a = 1, b = 2, c =3;
-
-let a, b, c ;
-a = 1;
-b = 2;
-c = 3;
+let b = 20;
+alert(b);
