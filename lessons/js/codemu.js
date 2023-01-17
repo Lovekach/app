279edd810a53b4fe8bd84fc10d0e4a412f1d24e2
LoveKach <cryptonitik@gmail.com>
2023-01-17 20:54:06 +0300

    codemu

--- lessons/js/codemu.js
+++ lessons/js/codemu.js
@@ -39,3 +39,28 @@ alert(d);
 
 let a = 1 + 2 + 3;
 alert(a);
+
+let a = 1;
+let b = 2;
+let c = a + b;
+alert(c);
+
+let a = 10;
+let b = 2;
+let c = a + b ;
+alert(c);
+
+let c = 10;
+let d = 5;
+let result = c + d;
+alert(result);
+
+
+let a = 10;
+let b = 5;
+let c = a - b;
+let d = 7;
+let result = c + d;
+alert(result);
+
+
