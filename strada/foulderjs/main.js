bbe8d1adf5eabfed8e838359a8dff9bd0a4c1038
LoveKach <cryptonitik@gmail.com>
2023-01-20 06:35:05 +0300

    strada

--- strada/foulderjs/main.js
+++ strada/foulderjs/main.js
@@ -4,3 +4,10 @@ let userHeight;
 userHeight = 170;
 console.log(userHeight);
 console.log(userName + ' is ' + userAge + ' years old');
+
+console.log(1 + 1);
+console.log(1 -1);
+console.log(4 / 2);
+console.log(3 * 3);
+
+console.log(`${3 + 3}`);
