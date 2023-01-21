c92f16d7fd03a5ddc57ac4dd92472884b137cad1
LoveKach <cryptonitik@gmail.com>
2023-01-21 08:11:14 +0300

    strada

---
+++ strada/folderjs/main2.js
@@ -0,0 +1,18 @@
+//Преобразование типов и базовые операторы
+
+console.log('1' + 1);
+console.log(22 + 'mango');
+console.log(true + true);
+
+const a = 3;
+const b = '2';
+console.log(Number(a) + Number(b));
+
+const a = 3;
+const b = '2a';
+console.log(a + b);
+console.log(Number(a) + Number(b));
+
+boolean = 0;
+console.log(boolean);
+
