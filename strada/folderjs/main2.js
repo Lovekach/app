79c6077a2f0c20918d5d80e6273e05c5f73201d9
LoveKach <cryptonitik@gmail.com>
2023-01-21 08:58:40 +0300

    strada

--- strada/folderjs/main2.js
+++ strada/folderjs/main2.js
@@ -16,3 +16,41 @@ console.log(Number(a) + Number(b));
 boolean = 0;
 console.log(boolean);
 
+
+typeof undefined//"undefined"
+typeof 6//"number"
+typeof true//"boolean"
+typeof 'user'// "string"
+
+
+let x = 1;
+x = -x;
+console.log(x); // -1 унарный оператор
+
+let age = '12';
+console.log(+age); // 12
+
+
+
+//Инкремент(увеличивает переменную на 1) и Декремент(уменьшает переменную на 1)
+ let counter = 2;
+ counter ++;
+ console.log(counter); // 3
+
+
+
+ const a = 3;
+ const b = 5;
+ console.log(a + b);
+
+ const a = 'week';
+ const b = 'year';
+ console.log(a + b);
+
+ const a = 3;
+ const b = 'day'
+ console.log(a +  b);
+
+ const a = 3;
+ const b = true;
+ console.log(a + b);
